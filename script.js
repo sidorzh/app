5f30554eaa1b2d17e527aeb05ecc7c13038810c0
Max <sidorzh@gmail.com>
2021-11-12 19:08:15 +0100

    created 3 questions to user

---
+++ script.js
@@ -0,0 +1,128 @@
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMOvieBD = {
+    count: numberOfFilms,
+    movies: {}, 
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMOvieBD.movies[a] = b;
+personalMOvieBD.movies[c] = d;
+
+console.log(personalMOvieBD);
+    
+
+
+// const isChecked = false,
+//     isClosed = false;
+
+// console.log(isChecked || !isClosed);
+
+// console.log(6 != 8);
+
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr; 
+// // --decr; 
+
+// console.log(++incr, --decr);
+
+// console.log(50 % 8);
+
+// &&
+
+
+
+
+
+// // console.log('apple ' + 'is fruit');
+
+// // console.log(1 + +'9');
+
+
+
+// const category = 'toys';
+
+// console.log('https://someurl.com/' + category + '/' + '4'); // это старый стандарт
+// console.log(`https://someurl.com/${category}/4`); // это интерполяция - 
+// //кавычки обызательно косые, а внутри - чтобы добавить переменную ставится ${переменная}
+// //и дальше без + остальной текст
+
+// const user = 'Max';
+
+// alert(`Hello, ${user}!`);
+
+// const answers = [];
+
+// answers[0] = prompt('Ваше имя?', '');
+// answers[1] = prompt('Фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(null));
+
+// const result = confirm('Are you here?');
+
+// const answer = prompt('How old are you', '');
+// console.log(typeof(answer));
+
+// alert('Hello');
+
+// console.log(4/0);
+// console.log('vasya' * 100);
+
+// const person = 'Alex';
+
+// console.log(syka);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj['name']);
+
+// // console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6];
+// console.log(arr[1]);
+
+
+
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// }
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
